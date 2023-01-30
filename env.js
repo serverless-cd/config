1e467c5fc433f18bd1621c5da01a970abf4be6dc
wss-git <wssgryx@163.com>
2023-01-30 09:29:49 +0800

    init repo
    Signed-off-by: wss-git <wssgryx@163.com>

--- env.js
+++ env.js
@@ -2,14 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const dotenv = require('dotenv');
 
-// 项目根目录 env 文件
-let envPath = path.join(__dirname, '..', '..', '..', '.env');
+// Serverless-Devs 目录的 env 文件
+let envPath = path.join(__dirname, '..', '..', '..', '..', '.env');
 if (fs.existsSync(envPath)) {
   dotenv.config({ path: envPath });
 }
 
-// Serverless-Devs 目录的 env 文件
-envPath = path.join(__dirname, '..', '..', '..', '..', '.env');
+// 项目根目录 env 文件
+envPath = path.join(__dirname, '..', '..', '..', '.env');
 if (fs.existsSync(envPath)) {
   dotenv.config({ path: envPath });
 }
