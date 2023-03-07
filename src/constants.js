e8583131e561bacdd67a3b8da53ba4d3ffc14022
wss-git <wssgryx@163.com>
2023-03-07 17:47:50 +0800

    skip webhookTriggered api
    Signed-off-by: wss-git <wssgryx@163.com>

--- src/constants.js
+++ src/constants.js
@@ -68,7 +68,7 @@ module.exports = {
   // webhook 事件支持
   WEBHOOK_EVENTS: ['push', 'pull_request'],
   // 不验证登陆的路由
-  EXCLUDE_AUTH_URL: ['/', '/api/auth/login', '/api/auth/signUp', '/api/common/init'],
+  EXCLUDE_AUTH_URL: ['/', '/api/auth/login', '/api/auth/signUp', '/api/common/init', '/api/common/webhookTriggered'],
   // 运行 engine 的函数名称
   WORKER_FUNCTION_NAME: 'worker',
 
