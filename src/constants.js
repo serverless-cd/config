18002a588370d8febab62821a5590fa913b97d4b
mhp01056531 <884103375@qq.com>
2023-03-26 03:19:48 +0800

    更新EXCLUDE_AUTH_URL

--- src/constants.js
+++ src/constants.js
@@ -71,7 +71,7 @@ module.exports = {
   // webhook 事件支持
   WEBHOOK_EVENTS: ['push', 'pull_request'],
   // 不验证登陆的路由
-  EXCLUDE_AUTH_URL: ['/', '/api/auth/login', '/api/auth/signUp', '/api/common/init', '/api/common/webhookTriggered', '/api/auth/callback/github', '/api/auth/callback/gitee', '/api/auth/callback/auth'],
+  EXCLUDE_AUTH_URL: ['/', '/api/auth/login', '/api/auth/signUp', '/api/common/init', '/api/common/webhookTriggered', '/api/auth/callback/github', '/api/auth/callback/gitee', '/api/auth/callback/auth', '/api/auth/updata'],
   // 运行 engine 的函数名称
   WORKER_FUNCTION_NAME: 'worker',
 
